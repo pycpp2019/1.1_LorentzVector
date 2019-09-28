622486269e961fdb56e62a8db52d4b827f125154
Nikolay Davydov <davnvl2000@gmail.com>
2019-09-28 18:42:00 +0700

    Introduced solution in main directory. Tmp dir for subtree is removed

---
+++ LorentzVector.cpp
@@ -0,0 +1,139 @@
+#include "LorentzVector.hpp"
+#include <iostream>
+#include <cmath>
+
+
+
+
+//##########___CONSTRUCTORS___#########################################################################################################
+
+
+LorentzVector::LorentzVector(): t(0), x(0), y(0), z(0) {
+
+}
+
+LorentzVector::LorentzVector(double input_t, double input_x, double input_y, double input_z) {
+
+	this->t = input_t;
+	this->x = input_x;
+	this->y = input_y;
+	this->z = input_z;
+}
+
+
+//##########___GETTERS___##############################################################################################################
+
+
+double LorentzVector::t() const {
+	return this->t;
+}
+
+double LorentzVector::x() const {
+	return this->x;
+}
+
+double LorentzVector::y() const {
+	return this->y;
+}
+
+double LorentzVector::z() const {
+	return this->z;
+}
+
+
+//##########___SETTERS___##############################################################################################################
+
+
+void LorentzVector::t(double new_t) {
+	this->t = new_t;
+	return;
+}
+
+void LorentzVector::x(double new_x) {
+	this->x = new_x;
+	return;
+}
+
+void LorentzVector::y(double new_y) {
+	this->y = new_y;
+	return;
+}
+
+void LorentzVector::z(double new_z) {
+	this->z = new_z;
+	return;
+}
+
+
+//##########___OUTPUTTING_METHOD___####################################################################################################
+
+
+void LorentzVector::print() const {
+	std::cout << '(' << LIGHT_VEL * this->t << ';' << this->x <<\
+		';' << this->y << ';' << this->z << ')' << std::endl;
+	return;
+}
+
+
+//##########___INPUTTING_METHOD___#####################################################################################################
+
+
+void read() {
+	std::cin >> this->t;
+	std::cin >> this->x;
+	std::cin >> this->y;
+	std::cin >> this->z;
+	return;
+}
+
+
+//##########___OPERATIONS___###########################################################################################################
+
+
+LorentzVector LorentzVector::add(const LorentzVector & other_vector) const {
+	return LorentzVector(this->t + other_vector.t(),\
+		this->x + other_vector.x(),\
+		this->y + other_vector.y(),\
+		this->z + other_vector.z());
+}
+
+LorentzVector LorentzVector::sub(const LorentzVector & other_vector) const {
+	return LorentzVector(this->t - other_vector.t(),\
+		this->x - other_vector.x(),\
+		this->y - other_vector.y(),\
+		this->z - other_vector.z());
+}
+
+LorentzVector LorentzVector::mul(double lambda) const {
+	return LorentzVector(lambda * this->t,\
+		lambda * this->x,\
+		lambda * this->y,\
+		lambda * this->z);
+}
+
+double LorentzVector::dot(const LorentzVector & other_vector) const {
+	return (this->t * other_vector.t() - this->x * other_vector.x() -\
+		this->y * other_vector.y() - this->z * other_vector.z());
+}
+
+double LorentzVector::norm() const {
+	return this->dot(*this);
+}
+
+
+//##########___CONVERSION_METHOD___####################################################################################################
+
+
+void LorentzVector::zboost(double beta) {									//If |beta| > 1, this function do nothing
+	if(beta * beta > 1) {
+		return;
+	}
+
+	double gamma_factor = 1 / sqrt(1 - beta * beta);
+	double tmp_t = this->t;
+	double tmp_z = this->z;
+	
+	this->z = gamma_factor * (tmp_z - beta * tmp_t);
+	this->t = gamma_factor * (tmp_t - beta * tmp_z);
+	return;
+}
--- LorentzVector.h
+++ LorentzVector.h
@@ -1,24 +1,38 @@
+#pragma once
+
 class LorentzVector {
+private:
+	double t;
+	double x;
+	double y;
+	double z;
+
 public:
-    LorentzVector();
-    LorentzVector(double, double, double, double);
-
-    double t() const;
-    double x() const;
-    double y() const;
-    double z() const;
-    double norm() const;
-
-    void t(double x);
-    void x(double x);
-    void y(double x);
-    void z(double x);
-
-    LorentzVector add(const LorentzVector& other) const;
-    LorentzVector sub(const LorentzVector& other) const;
-    LorentzVector mul(double a) const;
-    void zboost(double beta);
-    double dot(const LorentzVector& other) const;
-    void read();
-    void print() const;
+//##########___CONSTRUCTORS___#########################################################################################################
+	LorentzVector();
+	LorentzVector(double input_t, double input_x,\
+				double input_y, double input_z);
+//##########___GETTERS___##############################################################################################################
+	double t() const;
+	double x() const;
+	double y() const;
+	double z() const;
+//##########___SETTERS___##############################################################################################################
+	void t(double new_t);
+	void x(double new_x);
+	void y(double new_y);
+	void z(double new_z);
+//##########___OUTPUTTING_METHOD___####################################################################################################
+	void print() const;
+//##########___INPUTTING_METHOD___#####################################################################################################
+	void read();
+//##########___OPERATIONS___###########################################################################################################
+	LorentzVector add(const LorentzVector & other_vector) const;
+	LorentzVector sub(const LorentzVector & other_vector) const;
+	LorentzVector mul(double Lambda) const;
+	double dot(const LorentzVector & other_vector) const;
+	double norm() const;
+//##########___CONVERSION_METHOD___####################################################################################################	
+	void zboost(double beta);		//If |beta| > 1, this function do nothing
+	
 };
