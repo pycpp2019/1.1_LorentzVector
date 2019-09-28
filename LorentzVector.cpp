2ad9fc329124ef5c112b2a623c563309d2e876dd
Nikolay Davydov <davnvl2000@gmail.com>
2019-09-28 19:12:09 +0700

    Norm-method now returns non-squared norm ignoring complex values

--- LorentzVector.cpp
+++ LorentzVector.cpp
@@ -117,7 +117,7 @@ double LorentzVector::dot(const LorentzVector & other_vector) const {
 }
 
 double LorentzVector::norm() const {
-	return this->dot(*this);
+	return sqrt(abs(this->dot(*this)));
 }
 
 
